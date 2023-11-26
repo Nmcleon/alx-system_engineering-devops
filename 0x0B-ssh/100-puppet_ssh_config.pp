bc040b268ca94034c589c2dbb83d88455871bfe8
Nmcleon <nmcleon10@gmail.com>
2023-11-26 02:27:59 -0800

    complete adv task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-# Puppet Manifest.
-# Using puppet to make changse in config file
+# Puppet Manifest making changse in config file
 
 file {'etc/ssh/ssh_config':
 	ensure => present,
 
 content =>"
-
 	#SSH CLIENT CONFIGURATION
 	host*
 	IdentifyFile ~/.ssh/school
