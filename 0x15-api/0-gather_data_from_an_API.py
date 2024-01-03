521e8d5ceeebc608653a2970fcb2d07ff31c3114
N <51336368+Nmcleon@users.noreply.github.com>
2024-01-03 18:35:49 +0200

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     sessionReq = requests.Session()
 
     employee_id = argv[1]
-    base_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
-    url_name  = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+    base_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
+    url_name  = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
 
     employee = sessionReq.get(base_url)
     employee_name = sessionReq.get(url_name)
