9965019fd9663a56224096ac5d0d376368db2dab
Nmcleon <nmcleon10@gmail.com>
2024-01-03 06:20:11 -0800

    fix pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,6 +6,7 @@ A script that fetches an employee's TODO list progress using a REST API
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
     base_url = 'https://jsonplaceholder.typicode.com'
     todos_url = f'{base_url}/todos?userId={employee_id}'
@@ -18,17 +19,25 @@ def get_employee_todo_progress(employee_id):
         todos = response.json()
         user = requests.get(user_url).json()
 
-        completed_tasks = [task['title'] for task in todos if task['completed']]
+        completed_tasks = [
+                task['title']
+                for task in todos
+                if task['completed']
+                ]
         num_completed_tasks = len(completed_tasks)
         total_tasks = len(todos)
 
-        print(f"Employee {user['name']} is done with tasks({num_completed_tasks}/{total_tasks}):")
+        print(
+                f"Employee {user['name']} "
+                f"is done with tasks({num_completed_tasks}/{total_tasks}):"
+                )
         for task in completed_tasks:
             print(f"\t{task}")
 
     except requests.RequestException as e:
         print(f"Error: {e}")
         sys.exit(1)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -42,4 +51,3 @@ if __name__ == "__main__":
         sys.exit(1)
 
     get_employee_todo_progress(int(employee_id))
-
