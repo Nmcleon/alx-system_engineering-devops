cba6ad0c638aa838a5e53cb2c9d4fd260ace5e43
Nmcleon <nmcleon10@gmail.com>
2024-01-03 07:48:56 -0800

    fix tab err

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -32,7 +32,7 @@ def get_employee_todo_progress(employee_id):
                 f"is done with tasks({num_completed_tasks}/{total_tasks}):"
                 )
         for task in completed_tasks:
-            print(f"\t{task}")
+            print("\t" + task)
 
     except requests.RequestException as e:
         print(f"Error: {e}")
